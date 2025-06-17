fabecf275f7c7b7f8dff3a934a118c89b0ac40f8
Shaurya Upadhyay <shaurya01836@gmail.com>
2025-06-17 14:29:48 +0530

    update navbar

--- Frontend/src/components/layout/Navbar.jsx
+++ Frontend/src/components/layout/Navbar.jsx
@@ -14,45 +14,30 @@ function Navbar() {
     <nav className=" bg-white px-6 py-4">
       <div className="w-full px-6 py-4 flex items-center justify-between border-y-2">
         <div className="flex items-center gap-2">
-          <GitGraphIcon className="w-6 h-6 text-primary hover:rotate-90 transition-all duration-200 ease-in-out" />
-          <h1 className="text-2xl font-bold font-heading1 text-primary">
+          <GitGraphIcon className="w-6 h-6 text-[#1b0c3f] hover:rotate-90 transition-all duration-200 ease-in-out" />
+          <h1 className="text-2xl font-bold font-heading1 text-[#1b0c3f]">
             HackZen
           </h1>
         </div>
 
-        <div className="hidden md:flex gap-6 ">
-          <Link
-            to="/"
-            className="text-gray-800 hover:text-primary font-medium transition-all duration-150 ease-in-out hover:rotate-3"
-          >
-            Home
-          </Link>
-          <Link
-            to="/hackathons"
-            className="text-gray-800 hover:text-primary hover:rotate-3 font-medium transition-all duration-150 ease-in-out"
-          >
-            Hackathons
-          </Link>
-          <Link
-            to="/more"
-            className="text-gray-800 hover:text-primary font-medium transition-all duration-150 ease-in-out hover:rotate-3"
-          >
-            More
-          </Link>
-          <Link
-            to="/about"
-            className="text-gray-800 hover:text-primary font-medium transition-all duration-150 ease-in-out hover:rotate-3"
-          >
-            About
-          </Link>
+        <div className="hidden md:flex gap-6">
+          {["Home", "Hackathons", "More", "About"].map((text, index) => (
+            <Link
+              key={index}
+              to={`/${text.toLowerCase()}`}
+              className="text-[#1b0c3f] hover:text-primary font-medium transition-all duration-150 ease-in-out hover:rotate-3"
+            >
+              {text}
+            </Link>
+          ))}
         </div>
 
         <div className="hidden md:flex gap-4 items-center">
-          <BellIcon className="w-6 h-6 text-primary hover:bg-primary hover:text-white transition-all duration-150 ease-in-out hover:rounded-xl hover:p-1 hover:scale-150" />
-          <User2Icon className="w-6 h-6 text-primary hover:bg-primary hover:text-white transition-all duration-150 ease-in-out hover:rounded-xl hover:p-1 hover:scale-150" />
-          <button className="px-4 py-2 bg-primary text-white rounded-3xl font-semibold flex gap-4 items-center hover:skew-x-6 transition-all duration-200 ease-in-out">
+          <BellIcon className="w-6 h-6 text-[#1b0c3f] hover:bg-[#1b0c3f] hover:text-white transition-all duration-150 ease-in-out hover:rounded-xl hover:p-1 hover:scale-150" />
+          <User2Icon className="w-6 h-6 text-[#1b0c3f] hover:bg-[#1b0c3f] hover:text-white transition-all duration-150 ease-in-out hover:rounded-xl hover:p-1 hover:scale-150" />
+          <button className="px-4 py-2 bg-[#1b0c3f] text-white rounded-3xl font-semibold flex gap-4 items-center hover:skew-x-6 transition-all duration-200 ease-in-out">
             Contact us{" "}
-            <ContactIcon className="w-10 h-8 bg-white rounded-full text-primary py-2 hover:bg-primary hover:text-white hover:border-2 transition-all duration-200 hover:py-1" />
+            <ContactIcon className="w-10 h-8 bg-white rounded-full text-[#1b0c3f] py-2 hover:bg-[#1b0c3f] hover:text-white hover:border-2 transition-all duration-200 hover:py-1" />
           </button>
         </div>
 
