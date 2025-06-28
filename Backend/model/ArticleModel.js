591cfb390d560bb03c24760b0c2fd6388da48e13
Shaurya Upadhyay <153098108+Shaurya01836@users.noreply.github.com>
2025-06-28 23:40:13 +0530

    Delete Backend directory

--- Backend/model/ArticleModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const ArticleSchema = require('../schema/ArticleSchema'); // ❌ no destructuring here
-
-const Article = mongoose.models.Article || mongoose.model('Article', ArticleSchema);
-
-module.exports = Article;
--- Backend/model/NewsletterModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const { NewsletterSchema } = require('../schema/NewsletterSchema'); // ❌ no destructuring here
-
-const Newsletter = mongoose.models.Newsletter || mongoose.model('Newsletter', NewsletterSchema);
-
-module.exports = Newsletter;
